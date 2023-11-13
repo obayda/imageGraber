48d5108d4f8a259a8953284ca5dd440ce61b2aeb
Obaydah Saad <obaydaha@menoramivt.co.il>
2023-11-14 01:02:01 +0200

    Edit: script changes

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # exit on errorset -o errexit
-
+# source: https://community.render.com/t/error-could-not-found-chromium/9848/2
 npm install
 # npm run build # uncomment if required
 
 # Store/pull Puppeteer cache with build cache
-if [[ ! -d $PUPPETEER_CACHE_DIR]]; then 
+if [ ! -d $PUPPETEER_CACHE_DIR ] ; then 
   echo "...Copying Puppeteer Cache from Build Cache" 
   cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
 else 
