3c1630cbc5a278167eb671d6e061479f244855f4
Obaydah Saad <obaydaha@menoramivt.co.il>
2023-11-13 22:49:42 +0200

    basic functionality

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-import puppeteer from 'puppeteer';
+const puppeteer = require('puppeteer');
 
 (async () => {
   // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
   // Navigate the page to a URL
-  await page.goto('https://developer.chrome.com/');
+  await page.goto('https://www.google.com/search?tbm=isch&q=7290014666337');
 
-  // Set screen size
-  await page.setViewport({width: 1080, height: 1024});
 
-  // Type into search box
-  await page.type('.search-box__input', 'automate beyond recorder');
+  const selector = '[jsname="sTFXNd"]'
 
-  // Wait and click on first result
-  const searchResultSelector = '.search-box__link';
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
+  await page.waitForSelector(selector);
+  const element = await page.$(selector);
+  await page.click(selector);
 
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector(
-    'text/Customize and automate'
-  );
-  const fullTitle = await textSelector?.evaluate(el => el.textContent);
+  const t = await element.evaluate((el) => el.href);
 
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
+  console.log({
+    t: decodeURIComponent(t?.split("imgurl=")[1]?.split("&tbnid")[0])
+  })
 
-  await browser.close();
 })();
