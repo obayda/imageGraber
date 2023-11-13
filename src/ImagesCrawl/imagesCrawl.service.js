5306d7a74c19ee38548a9a85bc163428a6d2f31c
Obaydah Saad <obaydaha@menoramivt.co.il>
2023-11-14 00:56:59 +0200

    Edit: selector changed

--- src/ImagesCrawl/imagesCrawl.service.js
+++ src/ImagesCrawl/imagesCrawl.service.js
@@ -3,22 +3,27 @@ const puppeteer = require('puppeteer');
 async function getImage(req) {
     const { barcode: query } = req?.body;
 
-    const browser = await puppeteer.launch({ headless: "new" });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
     await page.goto(`https://www.google.com/search?tbm=isch&q=${query}`);
 
-    const selector = '[jsname="sTFXNd"]'
+    const selector = '[tabindex="0"]'
 
-    await page.waitForSelector(selector);
-    const element = await page.$(selector);
-    await page.click(selector);
+    await page.waitForSelector(selector, { timeout: 3000 });
+    const elements = await page.$$(selector);
 
-    const t = await element.evaluate((el) => el.href);
-    const w = decodeURIComponent(t?.split("imgurl=")[1]?.split("&tbnid")[0]);
+    let link;
 
-    return w;
+    for (const [index, element] of elements.entries()) {
+        if (index === 17) {
+            await element.click();
+            link = await element?.evaluate((el) => el.href);
+        }
+    }
 
+    const decoded = decodeURIComponent(link?.split("imgurl=")[1]?.split("&tbnid")[0]);
+    return decoded;
 }
 
 module.exports = {
