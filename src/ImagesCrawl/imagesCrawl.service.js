1305b814f5c1f4ed30ae35727a78df8837d84a80
Obaydah Saad <obaydaha@menoramivt.co.il>
2023-11-14 12:51:40 +0200

    Add: skip accept cookies

--- src/ImagesCrawl/imagesCrawl.service.js
+++ src/ImagesCrawl/imagesCrawl.service.js
@@ -1,37 +1,43 @@
 const puppeteer = require('puppeteer');
 
 async function getImage(req) {
-    const { barcode: query } = req?.body;
+    try {
+        const { barcode: query } = req?.body;
 
-    const browser = await puppeteer.launch({ headless: "new" });
+        const browser = await puppeteer.launch({ headless: "new" });
 
-    const page = await browser.newPage();
+        const page = await browser.newPage();
 
-    await page.goto(`https://www.google.com/search?tbm=isch&q=${query}`);
+        const url = `https://www.google.com/search?tbm=isch&q=${query}`;
 
-    const data = await page.content()
+        await page.goto(url, { waitUntil: "networkidle0" });
 
-    return data;
+        const acceptCookiesSelector = '[jsname="b3VHJd"]';
+        const imageSelector = '[jsname="sTFXNd"]';
 
-    // const selector = '[tabindex="0"]'
+        try {
+            await page.click(acceptCookiesSelector);
+        } catch (error) {
+            console.log({
+                acceptCookiesError: error
+            })
+        }
 
-    // await page.waitForSelector(selector, { timeout: 3000 });
-    // const elements = await page.$$(selector);
+        const selectImage = await page.$(imageSelector);
+        await page.click(imageSelector);
 
-    // let link;
+        const link = await selectImage.evaluate((el) => el.href);
+        const decoded = decodeURIComponent(link?.split("imgurl=")[1]?.split("&tbnid")[0]);
 
-    // for (const [index, element] of elements.entries()) {
-    //     if (index === 17) {
-    //         await element.click();
-    //         link = await element?.evaluate((el) => el.href);
-    //     }
-    // }
-
-    // const decoded = decodeURIComponent(link?.split("imgurl=")[1]?.split("&tbnid")[0]);
-    // return decoded;
+        return decoded;
+    } catch (error) {
+        console.log({
+            getImageError: error
+        })
+    }
 }
 
-// (async()=>{
+// (async () => {
 //     await getImage({
 //         body: {
 //             barcode: "7290006651730"
