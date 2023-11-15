075e6fd60743abc1cd37474b5cd317e7d9f20eb1
Obaydah Saad <obaydaha@menoramivt.co.il>
2023-11-16 00:30:15 +0200

    Add: logs

--- src/ImagesCrawl/imagesCrawl.route.js
+++ src/ImagesCrawl/imagesCrawl.route.js
@@ -6,7 +6,9 @@ router.route('/').post(imagesCrawl);
 
 async function imagesCrawl(req, res) {
     try {
+        console.log(`getting image for ${JSON.stringify(req.body)}`);
         const response = await getImage(req);
+        console.log(`getting image success`);
         return res.json({ ok: 1, data: response })
     } catch (error) {
         console.log(error);
